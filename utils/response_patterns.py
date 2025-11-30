506f39931367e1fbe3a5e737425635458a1ea216
N4MER <128246447+N4MER@users.noreply.github.com>
2025-11-30 19:38:51 +0100

    feat Add more response patterns

--- utils/response_patterns.py
+++ utils/response_patterns.py
@@ -8,8 +8,14 @@ class ResponsePatterns:
 
     PRIVILEGED_EXEC_MODE: Pattern[str] = re.compile(r'^[^\n\r]*#$', re.MULTILINE)
 
-    GLOBAL_CONFIG: Pattern[str] = re.compile(r'^\(config\)#$', re.MULTILINE)
+    GLOBAL_CONFIGURATION_MODE: Pattern[str] = re.compile(r'^\(config\)#$', re.MULTILINE)
 
     INITIAL_SETUP_MESSAGE: Pattern[str] = re.compile(r'^(Would you like to enter the initial configuration dialog\?)',re.MULTILINE | re.IGNORECASE)
 
-    #interface config mode
+    LINE_CONFIGURATION_MODE: Pattern[str] = re.compile(r'^\([^\n\r]*-line\)#$', re.MULTILINE)
+
+    INTERFACE_CONFIGURATION_MODE: Pattern[str] = re.compile(r'^\([^\n\r]*-if\)#$', re.MULTILINE)
+
+    ROUTER_CONFIGURATION_MODE: Pattern[str] = re.compile(r'^\([^\n\r]*-router\)#$', re.MULTILINE)
+
+    SUB_INTERFACE_CONFIGURATION_MODE: Pattern[str] = re.compile(r'^\([^\n\r]*-subif\)#$', re.MULTILINE)
