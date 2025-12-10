4b54c9d730005e5312be2d2cec203e1e19cb8c6f
N4MER <128246447+N4MER@users.noreply.github.com>
2025-12-10 19:01:23 +0100

    feat Add function for checking current mode.

--- serial_connection_manager.py
+++ serial_connection_manager.py
@@ -8,12 +8,10 @@ from serial.serialutil import SerialException
 
 from utils.exceptions import IncorrectResponseException
 
-LOG_LEVEL = logging.DEBUG
-
-logging.basicConfig(stream=sys.stdout, level=LOG_LEVEL, format="%(asctime)s [%(levelname)s] %(message)s")
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s")
 logger = logging.getLogger("serial_connection")
 
-class SerialConnection:
+class SerialConnectionManager:
 
     def __init__(self):
         self._port = None
@@ -72,19 +70,47 @@ class SerialConnection:
             logger.critical("Unexpected error during serial connection setup: %s", e)
             return None
 
-
-    def read_until_expected_output(self, expected_response: Pattern[str], read_timeout: float = 5, clear_buffer: bool = True) -> bool:
+    def read_output(self, read_timeout: float = 5):
         """
-        Reads data from the serial connection until there is no data read for the duration of read_timeout or until the expected response is received.
-        :param expected_response: Expected response.
-        :param clear_buffer: If true, clears the buffer before reading.
-        :param read_timeout: Reading stops if no new data is received from the device for this duration (default: 3s).
-        :return: True if the expected response matches.
+        Read output from  serial connection until no output read for the duration of read_timeout.
+        :param read_timeout: Read timeout.
+        :return: Read output.
         """
         logger.info(f"Starting Read from serial port {self._port}")
 
-        if clear_buffer:
-            self._clear_buffer()
+        output = ""
+        read_amount = 4096
+
+        last_data_time = time.time()
+
+        while True:
+            bytes_waiting = self._connection.in_waiting
+
+            if bytes_waiting > 0:
+
+                last_data_time = time.time()
+                data_bytes = self._connection.read(min(bytes_waiting, read_amount))
+                output += data_bytes.decode('utf-8', errors='ignore')
+
+            else:
+                time_since_last_data = time.time() - last_data_time
+
+                if time_since_last_data >= read_timeout:
+                    logger.info("No data received for %s seconds, stopping read.", read_timeout)
+                    break
+
+                time.sleep(0.1)
+        logger.info("stopped read")
+        return output
+
+    def read_until_expected_output(self, expected_response: Pattern[str], read_timeout: float = 5) -> bool:
+        """
+        Reads output from the serial connection until there is no data read for the duration of read_timeout or until the expected response is received.
+        :param expected_response: Expected response.
+        :param read_timeout: Reading stops if no new data is received from the device for this duration (default: 5s).
+        :return: True if the expected response matches.
+        """
+        logger.info(f"Starting Read from serial port {self._port}")
 
         output = ""
         read_amount = 4096
@@ -115,9 +141,11 @@ class SerialConnection:
         return False
 
 
-    def send(self, command: str | None = None, expected_response: Pattern[str] | None = None):
+
+    def send_command(self, command: str | None = None, expected_response: Pattern[str] | None = None, read_timeout: float = 5):
         """
-        Sends data to the serial connection.
+        Sends data to the serial connection and checks if output matches the expected_response regex.
+        :param read_timeout: Read timeout.
         :param command: Sent command.
         :param expected_response: Expected response.
         :return: True if the expected response matches.
@@ -131,12 +159,15 @@ class SerialConnection:
 
         self._connection.write((command_to_send + '\n').encode())
 
-        is_response_correct = self.read_until_expected_output(expected_response)
+        if expected_response is not None:
+            is_response_correct = self.read_until_expected_output(expected_response, read_timeout)
 
-        if not is_response_correct:
-            raise IncorrectResponseException("Incorrect response received from serial port.")
+            if not is_response_correct:
+                raise IncorrectResponseException("Incorrect response received from serial port.")
+
 
         logger.info(f"Successfully sent {command} to serial port {self._port}")
+
 
     def close_connection(self):
         """
@@ -144,3 +175,13 @@ class SerialConnection:
         :return:
         """
         self._connection.close()
+
+    def check_mode(self):
+        """
+        Sends an empty command to the serial connection then reads and returns the output.
+        :return: Output from sending empty command.
+        """
+        self._connection.write(b'\n')
+        time.sleep(0.1)
+        mode = self.read_output(1.0)
+        return mode
